2a97725224bb671a32de1cdf7a3a57642a596f87
Damir Cavar <damir.cavar@gmail.com>
2016-09-09 13:35:06 -0400

    added a final version of the CFG-grammar in LBNF and the corresponding test grammars, notes, etc.

---
+++ src/grammars/CFG/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# (C) 2016 by Damir Cavar
+
+TAG=cfg
+FNAME=CFG
+make clean
+bnfc -m --latex -p ${TAG} ${FNAME}.cf
+make
+bnfc -m -cpp -p ${TAG} ${FNAME}.cf
+sed -i -e 's/%name-prefix="/%name-prefix "/' ${FNAME}.y
+sed -i -e 's/<YYINITIAL>"\\\\n"/<YYINITIAL>"\\n"/' ${FNAME}.l
+sed -i -e 's/  extern char \*yytext;/  extern char \*'"$TAG"'yytext;/' ${FNAME}.y
+sed -i -e 's/    yy_mylinenumber, str, yytext);/    '"$TAG"'yy_mylinenumber, str, '"$TAG"'yytext);/' ${FNAME}.y
+make
